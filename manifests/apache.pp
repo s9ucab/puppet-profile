39ce6314cfb84232e45434680a17c1ae5ba32c8e
s9ucab <cbullock25@yahoo.com>
2017-11-08 23:16:19 +0000

    first commit

---
+++ manifests/apache.pp
@@ -0,0 +1,20 @@
+class profile::apache( 
+  Boolean $default_vhost = false,
+  Hash $apache_vhost_servers,
+
+#  Integer $port          = 80,
+#  String $docroot        = '/var/www/test',
+
+) {
+  class { '::apache':
+    default_vhost => $default_vhost,
+  }
+
+  create_resources(::apache::vhost, $apache_vhost_servers)
+
+#  ::apache::vhost { 's9ucab4.mylabserver.com':
+#    port    => $port,
+#    docroot => $docroot,
+#  }
+
+}
